ab4e4fd9dd700a476cf9f4a4a14590719d258c99
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-02-06 17:19:38 +0200

    interface for Gifts was added

---
+++ M2HW3/IGift.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2HW3
+{
+    public interface IGift
+    {
+        Candy[] Candies { get; }
+        decimal GetTotalWeight();
+        void AddCandy(Candy candy);
+        void DeleteCandy(string name);
+        void GetDescriptionGift();
+    }
+}
